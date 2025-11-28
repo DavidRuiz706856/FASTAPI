a5d2f3096d29e0dd9d3b09074b354bb004e64ca1
david ruiz <alumno.706856@ies-azarquiel.es>
2025-11-28 20:19:58 +0100

    tarea 1.3 terminada

--- practica1.3/main.py
+++ practica1.3/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import pandas as pd
 
-df = pd.read_csv("/practica1.3/datos_alumnos.csv")
+df = pd.read_csv("uno.csv", encoding="latin-1")
+df.columns = df.columns.str.lower().str.strip()
 
 app = FastAPI(title="iesazarquiel")
 
@@ -14,57 +15,41 @@ def info_alumnos():
 def asistencia(id: int = None):
     if id is None:
         return {
-            "mensaje": "este endpoint recibe 1 parámetro opcional llamado id",
-            "uso": "/asistencia?id=ID_DEL_ALUMNO",
-            "ejemplo": "/asistencia?id=3",
-            "ids_disponibles": df["id"].tolist()
+            "error no has metido el id"
         }
 
     if id not in df["id"].values:
-        return {
-            "error": "id no encontrado",
-            "ids_validos": df["id"].tolist()
-        }
+        return {"error ese alumno no existe"}
 
     alumno = df[df["id"] == id].iloc[0]
     return {
-        "id": id,
         "nombre": alumno["nombre"],
         "apellidos": alumno["apellidos"],
-        "asistencia_porcentaje": alumno["asistencia"]
+        "asistencia": alumno["asistencia"]
     }
 
 @app.get("/notas")
 def notas(id: int = None, nota: str = None):
-    if id is None and nota is None:
+    if id is None or nota is None:
+        notas_disponibles = [col for col in df.columns if col not in ["id", "nombre", "apellidos", "asistencia"]]
         return {
-            "mensaje": "este endpoint recibe parámetros opcionales: id y nota",
-            "parametro_id": "id del alumno",
-            "parametro_nota": "nombre de la columna de nota",
-            "notas_disponibles": [col for col in df.columns if col.startswith("nota")],
-            "ejemplos": [
-                "/notas?id=2&nota=nota_examen",
-                "/notas?id=1&nota=nota_practicas"
-            ]
-        }
+            "error falta algun parametro"
+            }
+        
 
-    if id is not None and id not in df["id"].values:
-        return {
-            "error": "id no encontrado",
-            "ids_validos": df["id"].tolist()
-        }
+    if id not in df["id"].values:
+        return {"error no existe el alumno ese alumno con ese id"}
 
-    if nota is not None and nota not in df.columns:
+    if nota not in df.columns:
+        notas_disponibles = [col for col in df.columns if col not in ["id", "nombre", "apellidos", "asistencia"]]
         return {
-            "error": "nombre de nota incorrecto",
-            "notas_validas": [col for col in df.columns if col.startswith("nota")]
+            "erroren la nota no existe"
         }
 
     alumno = df[df["id"] == id].iloc[0]
     return {
-        "id": id,
         "nombre": alumno["nombre"],
         "apellidos": alumno["apellidos"],
-        "nota_consultada": nota,
-        "valor": alumno[nota]
+        "nota": nota,
+        "calificacion": alumno[nota]
     }
